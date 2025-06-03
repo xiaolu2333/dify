7ea68ab69a7ae0b637ee21f59fec1582748080ee
xiaolu2333 <2694977226@qq.com>
2025-06-03 11:02:12 +0800

    pref: 去除底部提示

--- app/(commonLayout)/apps/page.tsx
+++ app/(commonLayout)/apps/page.tsx
@@ -1,22 +1,22 @@
 'use client'
-import { useTranslation } from 'react-i18next'
-import { RiDiscordFill, RiGithubFill } from '@remixicon/react'
+import {useTranslation} from 'react-i18next'
+import {RiDiscordFill, RiGithubFill} from '@remixicon/react'
 import Link from 'next/link'
 import style from '../list.module.css'
 import Apps from './Apps'
-import { useEducationInit } from '@/app/education-apply/hooks'
-import { useGlobalPublicStore } from '@/context/global-public-context'
+import {useEducationInit} from '@/app/education-apply/hooks'
+import {useGlobalPublicStore} from '@/context/global-public-context'
 
 const AppList = () => {
-  const { t } = useTranslation()
+  const {t} = useTranslation()
   useEducationInit()
-  const { systemFeatures } = useGlobalPublicStore()
+  const {systemFeatures} = useGlobalPublicStore()
   return (
     <div className='relative flex h-0 shrink-0 grow flex-col overflow-y-auto bg-background-body'>
-      <Apps />
+      <Apps/>
       {!systemFeatures.branding.enabled && <footer className='shrink-0 grow-0 px-12 py-6'>
-        <h3 className='text-gradient text-xl font-semibold leading-tight'>{t('app.join')}</h3>
-        <p className='system-sm-regular mt-1 text-text-tertiary'>{t('app.communityIntro')}</p>
+        {/*<h3 className='text-gradient text-xl font-semibold leading-tight'>{t('app.join')}</h3>*/}
+        {/*<p className='system-sm-regular mt-1 text-text-tertiary'>{t('app.communityIntro')}</p>*/}
         {/*<div className='mt-3 flex items-center gap-2'>*/}
         {/*  <Link className={style.socialMediaLink} target='_blank' rel='noopener noreferrer' href='https://github.com/langgenius/dify'>*/}
         {/*    <RiGithubFill className='h-5 w-5 text-text-tertiary' />*/}
@@ -26,7 +26,7 @@ const AppList = () => {
         {/*  </Link>*/}
         {/*</div>*/}
       </footer>}
-    </div >
+    </div>
   )
 }
 
